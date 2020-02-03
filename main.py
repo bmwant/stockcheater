5a8f8efeb39d3b903d43eccf3928a3189d2f9adb
Misha Behersky <bmwant@gmail.com>
2020-02-03 14:33:52 +0200

    Init engine once :older_man:

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 
+import chess
+from chess import engine as chess_engine
 from PyQt5 import QtGui, QtCore, uic
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import (
@@ -50,6 +52,8 @@ class MainWindow(QMainWindow):
         self.point_from = None
         self.point_to = None
 
+        self.engine = chess_engine.SimpleEngine.popen_uci(config.ENGINE_PATH)
+
     def mousePressEvent(self, event):
         self.hide()
         # QtWidgets.qApp.quit()
@@ -65,24 +69,20 @@ class MainWindow(QMainWindow):
         painter.setPen(QPen(Qt.blue, 2, Qt.SolidLine))
         painter.drawRect(0, 0, config.BOARD_SIZE, config.BOARD_SIZE)
 
-    def activate(self):
+    def show_move(self):
+        self.suggest_move()
         self.show()
 
-    def shot(self):
-        import chess
-        from chess import engine
+    def suggest_move(self):
         filename = grab_pieces.shot_board(QtWidgets.qApp)
         board_arr = self.recognizer.recognize_board(filename)
         board_fen = fen.create_fen(board_arr, white=self.is_white)
         board = chess.Board(fen=board_fen)
-        eng = engine.SimpleEngine.popen_uci(config.ENGINE_PATH)
-        result = eng.play(board, engine.Limit(time=0.1))
+        result = self.engine.play(board, chess_engine.Limit(time=0.1))
         line = fen.uci_move_to_line(result.move.uci())
         self.point_from = QPoint(*line.from_)
         self.point_to = QPoint(*line.to_)
         self.update()
-        self.show()
-        eng.quit()
 
     def set_white(self):
         self.is_white = True
@@ -113,14 +113,13 @@ def init_application():
     color_menu.addAction(mywindow.black_checkbox)
 
     show_action = QAction('Show move')
-    shot_action = QAction('Shot')
+    nothing_action = QAction('Nothing')
     exit_action = QAction('Exit')
     menu.addAction(show_action)
     menu.addMenu(color_menu)
-    menu.addAction(shot_action)
+    menu.addAction(nothing_action)
     menu.addAction(exit_action)
-    show_action.triggered.connect(mywindow.activate)
-    shot_action.triggered.connect(mywindow.shot)
+    show_action.triggered.connect(mywindow.show_move)
     exit_action.triggered.connect(QtWidgets.qApp.quit)
 
     # Add the menu to the tray
